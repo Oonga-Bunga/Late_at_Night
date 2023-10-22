215eb5243a4b6f9900a2aff90ff9c680716e8178
zdpgonzalo <zdpgonzalo@gmail.com>
2023-10-22 13:40:57 +0200

    MainMenu to LevelScene
    Ahora el menú principal guia al menú del nivel

--- GoodNightMrBunny_v0.2/Assets/Scripts/UI Scripts/MenuManager.cs
+++ GoodNightMrBunny_v0.2/Assets/Scripts/UI Scripts/MenuManager.cs
@@ -46,7 +46,7 @@ public class MenuManager : MonoBehaviour
     /// </summary>
     public void PressPlayButton()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("LevelScene");
     }
 
     /// <summary>
