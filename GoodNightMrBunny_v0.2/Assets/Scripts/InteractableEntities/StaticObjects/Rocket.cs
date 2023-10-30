6f9d349e5062ab2efd4116723b93afc0fac9feb7
Ubermewtwo <86959459+Ubermewtwo@users.noreply.github.com>
2023-10-30 18:52:10 +0100

    El proyectil del cohete ahora daña a los enemigos al explotar

--- GoodNightMrBunny_v0.2/Assets/Scripts/InteractableEntities/StaticObjects/Rocket.cs
+++ GoodNightMrBunny_v0.2/Assets/Scripts/InteractableEntities/StaticObjects/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] private float _blastDamage = 5f;
     [SerializeField] private float _blastRadius = 5f;
     [SerializeField] private float _speed = 5f; // Velocidad de movimiento hacia arriba
     [SerializeField] private GameObject _explosionPrefab = null; // Prefab del efecto de explosión
     [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private LayerMask _enemyLayer;
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +37,11 @@ public class Rocket : MonoBehaviour
     {
         // Crea el efecto de explosión en la posición del objeto
         Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, -_blastRadius, _enemyLayer);
+        foreach (Collider collider in hitColliders)
+        {
+            collider.gameObject.GetComponent<AMonster>().TakeHit(_blastDamage, GameManager.AttackSource.Rocket);
+        }
 
         // Destruye el objeto actual
         Destroy(gameObject);
