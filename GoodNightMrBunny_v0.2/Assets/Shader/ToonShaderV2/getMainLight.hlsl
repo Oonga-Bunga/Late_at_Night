0b5382a55b8b7f266cf75487b38a7728354bddb0
NatillasConNata <nataliamartinezclemente@hotmail.es>
2023-12-08 20:15:07 +0100

    Actualización tags enemigos, posición de algunos objetos conflictivos y luces backeadas

--- GoodNightMrBunny_v0.2/Assets/Shader/ToonShaderV2/getMainLight.hlsl
+++ GoodNightMrBunny_v0.2/Assets/Shader/ToonShaderV2/getMainLight.hlsl
@@ -11,11 +11,11 @@ void GetMainLightData_float(out half3 direction, out half3 color)
 #ifdef SHADERGRAPH_PREVIEW
     direction = half3(-0.3, -0.8, 0.6);
     color = half3(1, 1, 1);
-#else
+/*#else
 #if defined(UNIVERSAL_LIGHTING_INCLUDED)
     Light mainLight = GetMainLight();
     direction = mainLight.direction;
-    color = mainLight.color;
+    color = mainLight.color;*/
 #endif
 }
 
